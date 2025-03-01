5bf0adfa329019c9d7d366f72a887324cb827c3b
ReynoldArun09 <bemine.arun74@gmail.com>
2025-03-01 20:10:45 +0530

    remove: server files

--- server/src/middlewares/index.ts
+++
@@ -1,9 +0,0 @@
-import { ValidationMiddleware } from "./validation-middleware";
-import { ErrorMiddleware } from "./error-middleware";
-import { AuthMiddleware } from "./auth-middleware";
-
-export {
-    ValidationMiddleware,
-    ErrorMiddleware,
-    AuthMiddleware
-}
--- server/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import authRoutes from "./auth-routes";
-import postRoutes from "./post-routes";
-import commentRoutes from "./comment-routes";
-export {
-    authRoutes,
-    postRoutes,
-    commentRoutes
-}
