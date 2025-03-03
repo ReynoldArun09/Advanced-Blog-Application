7afe0592810f9905e3776ac81f0821091d504e55
ReynoldArun09 <bemine.arun74@gmail.com>
2025-03-03 15:14:10 +0530

    chore: added docker/docker-compose

--- backend-node/src/lib/redis.ts
+++ backend-node/src/lib/redis.ts
@@ -4,7 +4,10 @@ let client: RedisClientType | null = null;
 
 export async function initializeRedisClient() {
   if (!client) {
-    client = createClient();
+    const redisHost = process.env.REDIS_HOST || "localhost";
+    client = createClient({
+      url: `redis://${redisHost}:6379`,
+    });
     client.on("error", (error) => {
       console.log(error);
     });
--- backend-node/src/utils/s3.ts
+++ backend-node/src/utils/s3.ts
@@ -16,7 +16,6 @@ export const putObject = async (file: any, fileName: any) => {
       return;
     }
     let url = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}`;
-    console.log(url);
     return { url, key: params.Key };
   } catch (error) {
     console.log(error);
